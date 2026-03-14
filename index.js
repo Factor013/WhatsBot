9373623eabffd335509f9f129c97c4305143ab2f
Factor013 <rafaelreys7@gmail.com>
2026-03-13 21:50:51 -0300

    Whasapp Bot Simple

---
+++ index.js
@@ -0,0 +1,25 @@
+const {Client, LocalAuth} = require('whatsapp-web.js');
+var qrcode = require('qrcode-terminal')
+const { getMessage } = require('./messages');
+
+
+const client = new Client({
+    authStrategy: new LocalAuth({
+        clientId:'session',
+        dataPath:''
+    })
+});
+
+client.on('qr', (qr) => {
+    console.log("Leia o QRCODE com o Whatsapp");
+    qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () =>{
+    console.log('Atendente Online!')
+});
+client.on("message", msg =>{
+    getMessage(msg)
+});
+
+client.initialize();
---
+++ messages.js
@@ -0,0 +1,9 @@
+
+
+function getMessage(msg){
+    if(msg.body == "oi"){
+        msg.reply("topilhas por 3 reais")
+    }
+}
+
+module.exports = { getMessage };
